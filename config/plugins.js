46a3fa394945f5267845554ee1f980efd255e4af
Lorddickson <lorddickson751@gmail.com>
2020-11-06 12:25:49 +0100

    Installed Cloudinary Plugin

---
+++ config/plugins.js
@@ -0,0 +1,13 @@
+module.exports = ({ env }) => ({
+    upload: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('AKIAWXOXD6RYKSDWH3VI'),
+        secretAccessKey: env('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'),
+        region: 'us-east-2',
+        params: {
+          Bucket: 'my-bucket',
+        },
+      },
+    },
+  });
